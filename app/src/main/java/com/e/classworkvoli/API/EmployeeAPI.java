673631de545f28b51f77d9704699553751199973
Shovan404 <shovanrj4@gmail.com>
2019-12-23 03:56:51 -0500

    Register

--- app/src/main/java/com/e/classworkvoli/API/EmployeeAPI.java
+++ app/src/main/java/com/e/classworkvoli/API/EmployeeAPI.java
@@ -1,12 +1,15 @@
 package com.e.classworkvoli.API;
 
 import com.e.classworkvoli.model.Employee;
+import com.e.classworkvoli.model.EmployeeCUD;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
@@ -18,6 +21,13 @@ public interface EmployeeAPI {
     @GET("employee/{empID}")
     Call<Employee> getEmployeeID(@Path("empID") int  empID);
 
+    @POST("create")
+    Call<Void> registerEmployee(@Body EmployeeCUD emp);
+
     @PUT("employee/{empID}")
-    Call<Void> updateEmployee(@Path("empID") int empID, @Body Employee)
+    Call<Void> updateEmployee(@Path("empID") int empID, @Body EmployeeCUD emp);
+
+    @DELETE("delete/{empID}")
+    Call<Void> deleteEmployee(@Path("empID") int empID);
+
 }
--- app/src/main/java/com/e/classworkvoli/Register.java
+++ app/src/main/java/com/e/classworkvoli/Register.java
@@ -3,12 +3,70 @@ package com.e.classworkvoli;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.e.classworkvoli.API.EmployeeAPI;
+import com.e.classworkvoli.model.EmployeeCUD;
+import com.e.classworkvoli.url.URL;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Register extends AppCompatActivity {
+
+    private EditText etName, etSalary, etAge;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etName=findViewById(R.id.etName);
+        etAge=findViewById(R.id.etAge);
+        etSalary=findViewById(R.id.etSalary);
+        btnRegister=findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
+    }
+
+    public void Register(){
+
+        String name= etName.getText().toString();
+        Float salary = Float.parseFloat(etSalary.getText().toString());
+        int age = Integer.parseInt(etAge.getText().toString());
+
+        EmployeeCUD employee = new EmployeeCUD(name,salary,age);
+        Retrofit retrofit= new Retrofit.Builder()
+                .baseUrl(URL.base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        EmployeeAPI employeeAPI = retrofit.create(EmployeeAPI.class);
+        Call<Void> voidCall= employeeAPI.registerEmployee(employee);
+
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(Register.this, "Sucessfully registered", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(Register.this, "PepeLaugh Error"+t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
